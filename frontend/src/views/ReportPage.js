93b9386db351ba6d7a558e17fd9e985260b9ad07
justine <49121606+Jus10Yap@users.noreply.github.com>
2022-12-11 23:45:54 -0500

    Update ReportPage.js

---
+++ frontend/src/views/ReportPage.js
@@ -0,0 +1,15 @@
+import { Card, Layout } from 'antd';
+
+const ReportPage = () => {
+    return ( 
+        <Layout>
+            <Card title="Report">
+                <p>
+                    No Current Report
+                </p>
+            </Card>
+        </Layout>
+     );
+}
+ 
+export default ReportPage;
